273d55417706ab0037c02abe0ac8802db64cd0ae
Jakob Frank <jakob.frank@redlink.at>
2022-11-11 10:28:38 +0100

    MORE-75: Extend Security-Config

--- studymanager/src/main/java/io/redlink/more/studymanager/configuration/WebSecurityConfiguration.java
+++ studymanager/src/main/java/io/redlink/more/studymanager/configuration/WebSecurityConfiguration.java
@@ -3,9 +3,12 @@ package io.redlink.more.studymanager.configuration;
 import io.redlink.more.studymanager.service.OAuth2AuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.context.annotation.RequestScope;
 
 @Configuration
@@ -15,22 +18,27 @@ public class WebSecurityConfiguration {
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // Basics
-        http
-                .csrf().and()
-                .cors().disable();
+        http.csrf();
+        http.cors().disable();
+
         // Restricted Paths
-        http
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/api", "/api/v1/me").permitAll()
                 .antMatchers("/api/v1/**").authenticated()
                 .anyRequest().permitAll();
+
+        // API-Calls should not be redirected to the login page, but answered with a 401
+        http.exceptionHandling()
+                .defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED), new AntPathRequestMatcher("/api/**"));
 
         // Logout Config
         http.logout()
                 .logoutSuccessUrl("/");
 
         // Enable OAuth2
-        http.oauth2Login();
+        http.oauth2Login()
+                // register oauth2-provider under this baseurl to simplify routing
+                .authorizationEndpoint().baseUri("/login");
         // Enable OAuth2 client_credentials flow (insomnia)
         http.oauth2ResourceServer().jwt();
 
