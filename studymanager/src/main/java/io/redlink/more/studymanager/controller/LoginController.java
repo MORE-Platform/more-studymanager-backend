6784d7e1721bbbc5e358344ad25123992bb1618e
Jakob Frank <jakob.frank@redlink.at>
2022-11-14 22:24:26 +0100

    MORE-123: Some Security/Login Tweaks
    * OAuth2AuthenticationService now also handles JWT
    * Added /login/init endpoint to allow the frontend triggering a login with correct redirect

--- studymanager/src/main/java/io/redlink/more/studymanager/controller/LoginController.java
+++ studymanager/src/main/java/io/redlink/more/studymanager/controller/LoginController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/login/init")
 public class LoginController {
 
-    @GetMapping @PostMapping
+    @GetMapping
     public ResponseEntity<Void> initAuth(@RequestParam(value = "target", defaultValue = "/") URI target) {
         // Spring-Magic handles authentication, just send the client back where it came from.
         return ResponseEntity.status(HttpStatus.FOUND)
