0a41008d27a7f88874868d4550cdd17271fe589f
Jakob Frank <jakob.frank@redlink.at>
2022-11-14 16:17:22 +0100

    [Cleanup] Remove JPA dependency, it's not used anymore.

--- studymanager/src/main/java/io/redlink/more/studymanager/model/generator/RandomTokenGenerator.java
+++ studymanager/src/main/java/io/redlink/more/studymanager/model/generator/RandomTokenGenerator.java
@@ -3,16 +3,14 @@
  */
 package io.redlink.more.studymanager.model.generator;
 
-import java.io.Serializable;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.hibernate.HibernateException;
-import org.hibernate.engine.spi.SharedSessionContractImplementor;
-import org.hibernate.id.IdentifierGenerator;
 
-public class RandomTokenGenerator {
+public final class RandomTokenGenerator {
 
     private static final int TOKEN_LENGTH = 8;
     private static final char[] ALLOWED_CHARS = "ABCDEFGHKLMPRSTUVWXYZ23456789".toCharArray();
+
+    private RandomTokenGenerator() {}
 
     public static String generate() {
         return RandomStringUtils.random(TOKEN_LENGTH, 0, 0, true, true, ALLOWED_CHARS);
