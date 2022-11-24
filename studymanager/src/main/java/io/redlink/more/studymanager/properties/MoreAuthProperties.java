03f873e38604fe397251e6c9b19e4f00591001b9
Jakob Frank <jakob.frank@redlink.at>
2022-11-24 11:41:18 +0100

    MORE-336: squash: enums

--- studymanager/src/main/java/io/redlink/more/studymanager/properties/MoreAuthProperties.java
+++ studymanager/src/main/java/io/redlink/more/studymanager/properties/MoreAuthProperties.java
@@ -1,6 +1,6 @@
 package io.redlink.more.studymanager.properties;
 
-import io.redlink.more.studymanager.model.MoreUser;
+import io.redlink.more.studymanager.model.PlatformRole;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "more.auth")
 public record MoreAuthProperties(
-        Map<MoreUser.Role, Set<String>> globalRoles
+        Map<PlatformRole, Set<String>> globalRoles
 ) {
 
     public MoreAuthProperties {
