4804a4ab74dc968790c234d16709f98f1b7587da
Jakob Frank <jakob.frank@redlink.at>
2022-11-25 09:41:05 +0100

    MORE-374: Some cleanups

--- studymanager/src/main/java/io/redlink/more/studymanager/model/transformer/UserInfoTransformer.java
+++ studymanager/src/main/java/io/redlink/more/studymanager/model/transformer/UserInfoTransformer.java
@@ -3,13 +3,8 @@
  */
 package io.redlink.more.studymanager.model.transformer;
 
-import io.redlink.more.studymanager.api.v1.model.PlatformRoleDTO;
 import io.redlink.more.studymanager.api.v1.model.UserInfoDTO;
 import io.redlink.more.studymanager.model.AuthenticatedUser;
-import io.redlink.more.studymanager.model.PlatformRole;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public final class UserInfoTransformer {
 
@@ -20,23 +15,7 @@ public final class UserInfoTransformer {
                 .name(user.fullName())
                 .email(user.email())
                 .institution(user.institution())
-                .roles(toPlatformRoles(user.roles()));
-    }
-
-    public static Set<PlatformRoleDTO> toPlatformRoles(Set<PlatformRole> roles) {
-        return roles.stream()
-                .map(UserInfoTransformer::toPlatformRole)
-                .filter(Objects::nonNull)
-                .collect(Collectors.toUnmodifiableSet());
-    }
-
-    public static PlatformRoleDTO toPlatformRole(PlatformRole role) {
-        return switch (role) {
-            case MORE_VIEWER -> PlatformRoleDTO.VIEWER;
-            case MORE_OPERATOR -> PlatformRoleDTO.OPERATOR;
-            case MORE_ADMIN -> PlatformRoleDTO.ADMIN;
-            default -> null;
-        };
+                .roles(RoleTransformer.toPlatformRolesDTO(user.roles()));
     }
 
 }
