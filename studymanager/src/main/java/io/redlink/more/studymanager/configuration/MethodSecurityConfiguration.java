3bf86f896673e87f8a996aa57f1b6b3c7777bd85
Mikolaj Luzak <mluzak@Redlink-Info-Screen.hq.redlink.at>
2023-01-10 16:19:52 +0100

    added methodsafetyconfiguration

---
+++ studymanager/src/main/java/io/redlink/more/studymanager/configuration/MethodSecurityConfiguration.java
@@ -0,0 +1,10 @@
+package io.redlink.more.studymanager.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+@Configuration
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+public class MethodSecurityConfiguration{
+}
