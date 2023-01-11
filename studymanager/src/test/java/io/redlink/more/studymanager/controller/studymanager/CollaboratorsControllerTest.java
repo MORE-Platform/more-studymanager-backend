5d0ce610045ab1efd0c57a32fbf884f2b2180fb1
Mikolaj Luzak <mluzak@Redlink-Info-Screen.hq.redlink.at>
2023-01-11 14:49:36 +0100

    MORE-460: more added stuff to tester

--- studymanager/src/test/java/io/redlink/more/studymanager/controller/studymanager/CollaboratorsControllerTest.java
+++ studymanager/src/test/java/io/redlink/more/studymanager/controller/studymanager/CollaboratorsControllerTest.java
@@ -1,4 +1,31 @@
 package io.redlink.more.studymanager.controller.studymanager;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.redlink.more.studymanager.service.OAuth2AuthenticationService;
+import io.redlink.more.studymanager.service.StudyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest({CollaboratorsApiV1Controller.class})
+@AutoConfigureMockMvc(addFilters = false)
 public class CollaboratorsControllerTest {
+
+    @MockBean
+    StudyService studyService;
+
+    @MockBean
+    OAuth2AuthenticationService oAuth2AuthenticationService;
+
+    @Autowired
+    ObjectMapper mapper,
+
+    @Autowired
+    private MockMvc mvc;
+
+    void testRoleAuthetification(){
+
+    }
 }
