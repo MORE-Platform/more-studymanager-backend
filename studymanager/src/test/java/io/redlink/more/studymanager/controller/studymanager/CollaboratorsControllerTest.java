b2b7de04bfa75f99c1f81a2a3016f4b26d24cf0a
Mikolaj Luzak <mluzak@Mikolajs-MacBook-Air.local>
2023-01-16 17:10:34 +0100

    MORE-460: Remote-test failure fixed

--- studymanager/src/test/java/io/redlink/more/studymanager/controller/studymanager/CollaboratorsControllerTest.java
+++ studymanager/src/test/java/io/redlink/more/studymanager/controller/studymanager/CollaboratorsControllerTest.java
@@ -1,26 +1,20 @@
 package io.redlink.more.studymanager.controller.studymanager;
 
 import io.redlink.more.studymanager.model.*;
-import io.redlink.more.studymanager.repository.StudyAclRepository;
 import io.redlink.more.studymanager.service.OAuth2AuthenticationService;
 import io.redlink.more.studymanager.service.StudyPermissionService;
 import io.redlink.more.studymanager.service.StudyService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.*;
 
@@ -30,8 +24,10 @@ import static org.springframework.security.test.web.servlet.setup.SecurityMockMv
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-@AutoConfigureMockMvc
+
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test-containers-flyway")
 class CollaboratorsControllerTest {
     @MockBean
     StudyPermissionService studyPermissionService;
