85dddc13ea9e69f3358722a81cdd05b1b4a69336
Mikolaj Luzak <mluzak@Redlink-Info-Screen.hq.redlink.at>
2023-01-17 13:19:46 +0100

    MORE-460:test_cleanup

--- studymanager/src/test/java/io/redlink/more/studymanager/controller/studymanager/CollaboratorsControllerTest.java
+++ studymanager/src/test/java/io/redlink/more/studymanager/controller/studymanager/CollaboratorsControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -27,7 +28,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @Testcontainers
-@ActiveProfiles("test-containers-flyway-mock")
+@DirtiesContext
+@ActiveProfiles("test-containers-flyway")
 class CollaboratorsControllerTest {
     @MockBean
     StudyPermissionService studyPermissionService;
