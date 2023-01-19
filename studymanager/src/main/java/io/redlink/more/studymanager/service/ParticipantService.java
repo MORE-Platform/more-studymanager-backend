80d6f84d61b95cbe649c019a8b249a931b2f1c9c
Mikolaj Luzak <mluzak@Redlink-Info-Screen.hq.redlink.at>
2023-01-19 15:02:53 +0100

    MORE-460: removed unused method from participantService

--- studymanager/src/main/java/io/redlink/more/studymanager/service/ParticipantService.java
+++ studymanager/src/main/java/io/redlink/more/studymanager/service/ParticipantService.java
@@ -2,33 +2,20 @@ package io.redlink.more.studymanager.service;
 
 import io.redlink.more.studymanager.model.Participant;
 import io.redlink.more.studymanager.model.Study;
-import io.redlink.more.studymanager.model.StudyRole;
-import io.redlink.more.studymanager.model.User;
 import io.redlink.more.studymanager.model.generator.RandomTokenGenerator;
 import io.redlink.more.studymanager.repository.ParticipantRepository;
-import java.util.EnumSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import org.springframework.stereotype.Service;
-
-import static io.redlink.more.studymanager.model.Participant.Status.ABANDONED;
-import static io.redlink.more.studymanager.model.Participant.Status.KICKED_OUT;
-import static io.redlink.more.studymanager.model.Participant.Status.LOCKED;
 
 @Service
 public class ParticipantService {
 
-    private static final Set<StudyRole> READ_ROLES = EnumSet.of(StudyRole.STUDY_ADMIN, StudyRole.STUDY_OPERATOR);
-    private static final Set<StudyRole> WRITE_ROLES = READ_ROLES;
-
     private final ParticipantRepository participantRepository;
-    private final StudyPermissionService studyPermissionService;
 
 
-    public ParticipantService(ParticipantRepository repository, StudyPermissionService studyPermissionService) {
+    public ParticipantService(ParticipantRepository repository) {
         this.participantRepository = repository;
-        this.studyPermissionService = studyPermissionService;
     }
 
     public Participant createParticipant(Participant participant) {
@@ -50,14 +37,6 @@ public class ParticipantService {
 
     public Participant updateParticipant(Participant participant) {
         return participantRepository.update(participant);
-    }
-
-    public void setStatus(Long studyId, Integer participantId, Participant.Status status, User user) {
-        studyPermissionService.assertAnyRole(studyId, user.id(), WRITE_ROLES);
-        participantRepository.setStatusByIds(studyId, participantId, status);
-        if (EnumSet.of(ABANDONED, KICKED_OUT, LOCKED).contains(status)) {
-            participantRepository.cleanupParticipant(studyId, participantId);
-        }
     }
 
     public void alignParticipantsWithStudyState(Study study) {
