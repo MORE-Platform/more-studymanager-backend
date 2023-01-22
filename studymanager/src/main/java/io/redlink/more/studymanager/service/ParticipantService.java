44b16b3c5f666cbbc2744b5e2feaaef8c2889804
Mikolaj Luzak <mluzak@Mikolajs-MacBook-Air.local>
2023-01-22 20:30:18 +0100

    MORE-460:added participant setStatus method back, removed redundant impl. of PermissionEvaluator

--- studymanager/src/main/java/io/redlink/more/studymanager/service/ParticipantService.java
+++ studymanager/src/main/java/io/redlink/more/studymanager/service/ParticipantService.java
@@ -2,20 +2,29 @@ package io.redlink.more.studymanager.service;
 
 import io.redlink.more.studymanager.model.Participant;
 import io.redlink.more.studymanager.model.Study;
+import io.redlink.more.studymanager.model.StudyRole;
+import io.redlink.more.studymanager.model.User;
 import io.redlink.more.studymanager.model.generator.RandomTokenGenerator;
 import io.redlink.more.studymanager.repository.ParticipantRepository;
+
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+
+import static io.redlink.more.studymanager.model.Participant.Status.*;
 
 @Service
 public class ParticipantService {
 
     private final ParticipantRepository participantRepository;
+    private final StudyPermissionService studyPermissionService;
 
 
-    public ParticipantService(ParticipantRepository repository) {
+    public ParticipantService(ParticipantRepository repository, StudyPermissionService studyPermissionService) {
         this.participantRepository = repository;
+        this.studyPermissionService = studyPermissionService;
     }
 
     public Participant createParticipant(Participant participant) {
@@ -42,6 +51,14 @@ public class ParticipantService {
     public void alignParticipantsWithStudyState(Study study) {
         if (study.getStudyState() == Study.Status.CLOSED) {
             participantRepository.cleanupParticipants(study.getStudyId());
+        }
+    }
+
+    public void setStatus(Long studyId, Integer participantId, Participant.Status status, User user) {
+        studyPermissionService.assertAnyRole(studyId, user.id(), EnumSet.of(StudyRole.STUDY_ADMIN, StudyRole.STUDY_OPERATOR));
+        participantRepository.setStatusByIds(studyId, participantId, status);
+        if (EnumSet.of(ABANDONED, KICKED_OUT, LOCKED).contains(status)) {
+            participantRepository.cleanupParticipant(studyId, participantId);
         }
     }
 }
