43a3168f509f18601e3ffe2a969cbf6d3d674b0e
Jakob Frank <jakob.frank@redlink.at>
2023-01-31 16:32:49 +0100

    Cleanup outdated and dangling push-notification token

--- studymanager/src/main/java/io/redlink/more/studymanager/service/PushNotificationService.java
+++ studymanager/src/main/java/io/redlink/more/studymanager/service/PushNotificationService.java
@@ -1,20 +1,20 @@
 package io.redlink.more.studymanager.service;
 
 import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.MessagingErrorCode;
 import io.redlink.more.studymanager.model.PushNotificationsToken;
 import io.redlink.more.studymanager.repository.PushNotificationTokenRepository;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class PushNotificationService {
     private final PushNotificationTokenRepository pushNotificationsRepository;
     private final FirebaseMessagingService firebaseService;
 
-    private static final Logger log = LoggerFactory.getLogger(PushNotificationService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(PushNotificationService.class);
 
     public PushNotificationService(PushNotificationTokenRepository pushNotificationsRepository,
                                    FirebaseMessagingService firebaseService) {
@@ -25,21 +25,32 @@ public class PushNotificationService {
     public boolean sendPushNotification(long studyID, int participantId, String title, String message) {
         Optional<PushNotificationsToken> tkn = this.pushNotificationsRepository.getTokenById(studyID, participantId);
 
-        if (tkn.isPresent()) {
-            PushNotificationsToken token = tkn.get();
-            if ("FCM".equals(token.service())) {
-                try {
-                    this.firebaseService.sendNotification(title, message, token.token());
-                    return true;
-                } catch (FirebaseMessagingException e) {
-                    log.warn("Could not send Notification (sid:{} pid{}): {}", studyID, participantId, e.getMessage(), e);
-                    return false;
-                }
-            }
-        } else {
-            log.debug("Did not send notification: No token for participant sid:{} pid:{} registered", studyID, participantId);
+        if (tkn.isEmpty()) {
+            LOG.debug("Can't send notification: No token for participant sid:{} pid:{} registered", studyID, participantId);
+            return false;
         }
 
-        return false;
+        final PushNotificationsToken token = tkn.get();
+        final String serviceType = token.service();
+        if ("FCM".equals(serviceType)) {
+            try {
+                this.firebaseService.sendNotification(title, message, token.token());
+                return true;
+            } catch (FirebaseMessagingException e) {
+                if (e.getMessagingErrorCode() == MessagingErrorCode.UNREGISTERED) {
+                    LOG.debug("Outdated FCM-Notification token (sid:{} pid:{}), trying to clean-up...", studyID, participantId);
+                    if (pushNotificationsRepository.deleteToken(studyID, participantId)) {
+                        LOG.info("Cleared outdated FCM-Notification token (sid:{} pid:{})", studyID, participantId);
+                    }
+                } else {
+                    LOG.warn("Could not send Notification (sid:{} pid:{}): {}", studyID, participantId, e.getMessage(), e);
+                }
+                return false;
+            }
+        } else {
+            LOG.warn("Unknown Notification-Service-Type: {} - cannot send notification (sid:{} pid:{})",
+                    serviceType, studyID, participantId);
+            return false;
+        }
     }
 }
