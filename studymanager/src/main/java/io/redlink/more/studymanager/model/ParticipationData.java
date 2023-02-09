99d168edc43d34f32a52c7774f19d95bf8c2f8ff
Mikolaj Luzak <mikolaj.luzak@redlink.at>
2023-02-09 13:33:26 +0100

    MORE-625: added titles/aliases to sent ParticipationData

--- studymanager/src/main/java/io/redlink/more/studymanager/model/ParticipationData.java
+++ studymanager/src/main/java/io/redlink/more/studymanager/model/ParticipationData.java
@@ -4,20 +4,34 @@ import java.time.Instant;
 import java.util.Comparator;
 
 public record ParticipationData(
-        Integer observationId,
-        Integer participantId,
-        Integer studyGroupId,
+        Pair observationData,
+        Pair participantData,
+        Pair studyGroupData,
         boolean dataReceived,
         Instant lastDataReceived
 ) implements Comparable<ParticipationData> {
 
     public static final Comparator<ParticipationData> PARTICIPATION_DATA_COMPARATOR =
-            Comparator.comparing(ParticipationData::observationId)
-                    .thenComparing(ParticipationData::studyGroupId)
-                    .thenComparing(ParticipationData::participantId);
+
+            Comparator.comparing(ParticipationData::observationData)
+                    .thenComparing(ParticipationData::studyGroupData)
+                    .thenComparing(ParticipationData::participantData);
 
     @Override
     public int compareTo(ParticipationData compParticipation) {
         return PARTICIPATION_DATA_COMPARATOR.compare(this, compParticipation);
+    }
+
+    public record Pair(
+            int id,
+            String title
+    ) implements Comparable<Pair>{
+
+        public static final Comparator<Pair> PAIR_COMPARATOR =
+                Comparator.comparing(Pair::id);
+        @Override
+        public int compareTo(Pair compData) {
+            return PAIR_COMPARATOR.compare(this, compData);
+        }
     }
 }
