b994e31f227d3fd6ccc8d27bd3610dffa8c00043
juliamayrhauser <julia.mayrhauser@redlink.at>
2023-02-22 12:41:55 +0100

    MORE-705 code refactorings

--- studymanager-observation/src/main/java/io/redlink/more/studymanager/component/observation/LimeSurveyRequestService.java
+++ studymanager-observation/src/main/java/io/redlink/more/studymanager/component/observation/LimeSurveyRequestService.java
@@ -6,16 +6,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import io.redlink.more.studymanager.component.observation.model.*;
 import io.redlink.more.studymanager.core.factory.ComponentFactoryProperties;
 
 import java.io.IOException;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -89,8 +82,8 @@ public class LimeSurveyRequestService {
                 );
             return mapper.readValue(
                     client.send(request, HttpResponse.BodyHandlers.ofString()).body(),
-                    LimeSurveyKeyResponse.class)
-                    .result();
+                    LimeSurveyObjectResponse.class)
+                    .result().toString();
         }catch(IOException | InterruptedException e){
             e.printStackTrace();
             return "";
@@ -100,36 +93,27 @@ public class LimeSurveyRequestService {
     protected String getUrl(){
         return properties.get("url").toString();
     }
+
     public JsonNode listSurveysByUser (String username, String filter, Integer start, Integer size){
         try {
             LimeSurveyRequest request = new LimeSurveyRequest("list_surveys", List.of(getSessionKey(), username), 1);
             HttpRequest listSurveysRequest = createHttpRequest(objectWriter.writeValueAsString(request));
-            LimeSurveyListResponse response =
+            LimeSurveyListSurveyResponse response =
                     mapper.readValue(client.send(listSurveysRequest, HttpResponse.BodyHandlers.ofString()).body(),
-                            LimeSurveyListResponse.class);
-            if (response.error() != null) {
-                return mapper.convertValue(response.result(), JsonNode.class);
-            }
-            ArrayNode studies = mapper.convertValue(response.result(), ArrayNode.class);
-            List<Map<String, Object>> transformedResult = new ArrayList<>();
-            studies.forEach(entry -> {
-                if (isInFilter(filter, entry.get("surveyls_title").asText())) {
-                    transformedResult
-                            .add(Map.of("surveyId", entry.get("sid").asText(),
-                                    "surveyTitle", entry.get("surveyls_title").asText()));
-                }
+                            LimeSurveyListSurveyResponse.class);
+            response.result().forEach(entry -> {
+                if (!isInFilter(filter, entry.surveyTitle())) { response.result().remove(entry); }
             });
-            List<Map<String, Object>> paginatedResult = handlePagination(transformedResult, start, size);
-            return mapper.convertValue(paginatedResult, JsonNode.class);
+            return mapper.convertValue(handlePagination(response.result(), start, size), JsonNode.class);
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private List<Map<String, Object>> handlePagination (List < Map < String, Object >> list, Integer start, Integer
+    private List<SurveyData> handlePagination (List <SurveyData> list, Integer start, Integer
     size){
         if (start != null && size != null) {
-            List<Map<String, Object>> sublist = list.subList(start, list.size());
+            List<SurveyData> sublist = list.subList(start, list.size());
             list = size < sublist.size() ? sublist.subList(0, size) : sublist;
         }
         return list;
@@ -143,6 +127,12 @@ public class LimeSurveyRequestService {
                         .BodyPublishers
                         .ofString(body))
                 .build();
+    }
+
+    protected String parseRequest(String method, List<Object> params, Integer id) throws JsonProcessingException {
+        return objectWriter.writeValueAsString(
+                new LimeSurveyRequest(method, params, id)
+        );
     }
 
     private boolean isInFilter (String filter, String text){
