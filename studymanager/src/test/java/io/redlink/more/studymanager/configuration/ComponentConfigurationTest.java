69815bfab94d1367050d161149e80736b8778bc8
Mikolaj Luzak <mikolaj.luzak@redlink.at>
2023-02-23 11:45:21 +0100

    MORE-706: LimeSurvey parameters configurability added

--- studymanager/src/test/java/io/redlink/more/studymanager/configuration/ComponentConfigurationTest.java
+++ studymanager/src/test/java/io/redlink/more/studymanager/configuration/ComponentConfigurationTest.java
@@ -1,7 +1,6 @@
 package io.redlink.more.studymanager.configuration;
 
 import io.redlink.more.studymanager.component.observation.LimeSurveyObservationFactory;
-import io.redlink.more.studymanager.core.factory.ObservationFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +8,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 
-import java.util.Map;
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        args = {
+        "--more.components.lime-survey-observation.url=https://testurl",
+                "--more.components.lime-survey-observation.username=testUsername",
+                "--more.components.lime-survey-observation.password=testPassword"})
 @ContextConfiguration(classes = ComponentFactoriesConfiguration.class)
 public class ComponentConfigurationTest {
 
     @Autowired
     ApplicationContext context;
+
     @Test
     public void testConfig() {
         LimeSurveyObservationFactory factory = context.getBean(LimeSurveyObservationFactory.class);
-        Assertions.assertEquals(factory.componentProperties.get("url"), "https://lime.platform-test.more.redlink.io/admin/remotecontrol");
+        Assertions.assertEquals(factory.componentProperties.get("username"), "testUsername");
+        Assertions.assertEquals(factory.componentProperties.get("password"), "testPassword");
+        Assertions.assertEquals(factory.componentProperties.get("url"), "https://testurl");
     }
 }
