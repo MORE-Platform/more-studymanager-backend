0bd7391fbbc7f78b0a89264bd652486b34f93036
Thomas Kurz <thomas.kurz@redbull.com>
2023-09-22 12:08:54 +0200

    revert wrongly adapted flyway file

--- studymanager/src/main/resources/db/migration/V1_7_0__observation_hidden_field.sql
+++ studymanager/src/main/resources/db/migration/V1_7_0__observation_hidden_field.sql
@@ -2,7 +2,7 @@ ALTER TABLE observations
     ADD COLUMN hidden BOOLEAN;
 
 UPDATE observations
-SET hidden = FALSE;
+    SET hidden = FALSE;
 
 ALTER TABLE observations
     ALTER COLUMN hidden SET NOT NULL;
