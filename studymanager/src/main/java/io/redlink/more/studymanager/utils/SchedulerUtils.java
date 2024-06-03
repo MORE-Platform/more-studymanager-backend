9d26cd865bb2e6ce00ec4c05894dadeb1be45b17
Mikolaj Luzak <92162006+drtyyj@users.noreply.github.com>
2024-06-03 15:59:50 +0200

    299 absolute obs until study end runtime calculation (#300)
    * change calculation of untilDate of absolute recurring observation
    
    * add calculation of study end for absolute recurring observations

--- studymanager/src/main/java/io/redlink/more/studymanager/utils/SchedulerUtils.java
+++ studymanager/src/main/java/io/redlink/more/studymanager/utils/SchedulerUtils.java
@@ -63,7 +63,7 @@ public final class SchedulerUtils {
     public static List<Range<Instant>> parseToObservationSchedulesForEvent(Event event, Instant start, Instant end) {
         List<Range<Instant>> observationSchedules = new ArrayList<>();
         if (event.getDateStart() != null && event.getDateEnd() != null) {
-            VEvent iCalEvent = parseToICalEvent(event);
+            VEvent iCalEvent = parseToICalEvent(event, end);
             long eventDuration = getEventTime(event);
             DateIterator it = iCalEvent.getDateIterator(TimeZone.getDefault());
             while (it.hasNext()) {
@@ -174,14 +174,16 @@ public final class SchedulerUtils {
         return java.time.Duration.between(event.getDateStart(), event.getDateEnd()).getSeconds();
     }
 
-    private static VEvent parseToICalEvent(Event event) {
+    private static VEvent parseToICalEvent(Event event, Instant fallBackEnd) {
         VEvent iCalEvent = new VEvent();
         iCalEvent.setDateStart(Date.from(event.getDateStart()));
         iCalEvent.setDateEnd(Date.from(event.getDateEnd()));
 
         RecurrenceRule eventRecurrence = event.getRRule();
-        if (event.getRRule() != null) {
+        if (eventRecurrence != null) {
             Recurrence.Builder recurBuilder = new Recurrence.Builder(Frequency.valueOf(eventRecurrence.getFreq()));
+
+            setUntil(recurBuilder, Objects.requireNonNullElse(eventRecurrence.getUntil(), fallBackEnd));
             setUntil(recurBuilder, eventRecurrence.getUntil());
             setCount(recurBuilder, eventRecurrence.getCount());
             setInterval(recurBuilder, eventRecurrence.getInterval());
