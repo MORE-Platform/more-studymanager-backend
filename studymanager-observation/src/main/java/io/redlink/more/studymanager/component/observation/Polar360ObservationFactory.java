c493a83124c4112a9bb70a7153bff9e793bd18fe
Gmasek <masek.gergely98@gmail.com>
2025-12-22 10:33:50 +0100

    Polar 360 with settings.

--- studymanager-observation/src/main/java/io/redlink/more/studymanager/component/observation/Polar360ObservationFactory.java
+++ studymanager-observation/src/main/java/io/redlink/more/studymanager/component/observation/Polar360ObservationFactory.java
@@ -8,7 +8,10 @@ import io.redlink.more.studymanager.core.exception.ConfigurationValidationExcept
 import io.redlink.more.studymanager.core.factory.ObservationFactory;
 import io.redlink.more.studymanager.core.measurement.MeasurementSet;
 import io.redlink.more.studymanager.core.properties.ObservationProperties;
+import io.redlink.more.studymanager.core.properties.model.BooleanValue;
+import io.redlink.more.studymanager.core.properties.model.ChoiceValue;
 import io.redlink.more.studymanager.core.properties.model.IntegerValue;
+import io.redlink.more.studymanager.core.properties.model.StringListValue;
 import io.redlink.more.studymanager.core.properties.model.Value;
 import io.redlink.more.studymanager.core.sdk.MoreObservationSDK;
 
@@ -20,7 +23,14 @@ public class Polar360ObservationFactory <C extends Observation<P>, P extends Obs
 
 
     private static List<Value> properties = List.of(
-        
+        new IntegerValue("sampling_rate").setDefaultValue(1)
+        .setDescription("Downsampling for online streaming"),
+        new BooleanValue("Offline_recording").setDefaultValue(false).setDescription("Offline recording on device or online streaming"),
+        new BooleanValue("Acc").setDefaultValue(false).setDescription("Collect acc data"),
+        new BooleanValue("Temp").setDefaultValue(false).setDescription("Collect temp data"),
+        new BooleanValue("Ppi").setDefaultValue(false).setDescription("Collect Ppi, if enabled, Hr cant be collected on online stream we collect ppi data"),
+        new BooleanValue("Hr").setDefaultValue(false).setDescription("Collect HR data, if Hr collected Ppi cant be if both toggled HR will be collected"),
+        new BooleanValue("continuous_recording").setDefaultValue(false).setDescription("Continous data collection with offline recording, restart recording after fetching records")
     ); 
 
 
