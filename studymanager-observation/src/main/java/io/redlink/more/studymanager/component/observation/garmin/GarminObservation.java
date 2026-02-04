402af1ddd5e1d3d96ad36cc29e733beda62cee46
Jan Cortiel <jan.cortiel@redlink.at>
2026-02-04 14:18:37 +0100

    #184: Readded original Garmin Observations for backward compatability

---
+++ studymanager-observation/src/main/java/io/redlink/more/studymanager/component/observation/garmin/GarminObservation.java
@@ -0,0 +1,13 @@
+package io.redlink.more.studymanager.component.observation.garmin;
+
+import io.redlink.more.studymanager.core.component.Observation;
+import io.redlink.more.studymanager.core.exception.ConfigurationValidationException;
+import io.redlink.more.studymanager.core.properties.ObservationProperties;
+import io.redlink.more.studymanager.core.sdk.MoreObservationSDK;
+
+@Deprecated
+public class GarminObservation<C extends ObservationProperties> extends Observation<C> {
+    protected GarminObservation(MoreObservationSDK sdk, C properties) throws ConfigurationValidationException {
+        super(sdk, properties);
+    }
+}
---
+++ studymanager-observation/src/main/java/io/redlink/more/studymanager/component/observation/garmin/GarminObservationFactory.java
@@ -0,0 +1,45 @@
+package io.redlink.more.studymanager.component.observation.garmin;
+
+import io.redlink.more.studymanager.core.component.Observation;
+import io.redlink.more.studymanager.core.exception.ConfigurationValidationException;
+import io.redlink.more.studymanager.core.factory.ObservationFactory;
+import io.redlink.more.studymanager.core.io.Visibility;
+import io.redlink.more.studymanager.core.measurement.MeasurementSet;
+import io.redlink.more.studymanager.core.properties.ObservationProperties;
+import io.redlink.more.studymanager.core.sdk.MoreObservationSDK;
+
+@Deprecated
+public class GarminObservationFactory<C extends Observation<P>, P extends ObservationProperties> extends ObservationFactory<C, P> {
+
+    private static final Visibility visibility = new Visibility(true, false);
+
+    @Override
+    public GarminObservation create(MoreObservationSDK sdk, ObservationProperties properties) throws ConfigurationValidationException {
+        return new GarminObservation(sdk, validate((P) properties));
+    }
+
+    @Override
+    public MeasurementSet getMeasurementSet() {
+        return MeasurementSet.None;
+    }
+
+    @Override
+    public String getId() {
+        return "garmin-observation";
+    }
+
+    @Override
+    public String getTitle() {
+        return "observation.factory.garmin.title";
+    }
+
+    @Override
+    public String getDescription() {
+        return "observation.factory.garmin.description";
+    }
+
+    @Override
+    public Visibility getVisibility() {
+        return visibility;
+    }
+}
